5c5f33f3841414475e632df1105d23cefbeed616
Yujin830 <rladbwls30@naver.com>
2022-02-07 01:58:50 +0900

    Start project

---
+++ js/main.js
@@ -0,0 +1,154 @@
+const searchEl=document.querySelector('.search');
+const searchInputEl=searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+  // Logic..
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+
+const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if(window.scrollY > 500){
+    //배치 숨기기
+    //gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,
+      display: 'none' //사라지면 선택 안되게 사라지게 만들어야 합니다.
+    }); 
+    //버튼 보이기
+    gsap.to(toTopEl, 0.2, {
+      x: 0
+    });
+  }
+  else{
+    //배치 보이기
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: 'block'
+    }); 
+    //버튼 숨기기
+    gsap.to(toTopEl, 0.2, {
+      x: 100
+    });
+  }
+}, 300));
+// _.throttle(함수, 몇초에 한번씩 실행하면 되는지 시간을 ms단위로 추가한다.)
+
+toTopEl.addEventListener('click', function() {
+  gsap.to(window, 0.7, {
+    scrollTo: 0 //html에 gsap ScrolTo 붙여넣기 해야만 작동
+  });
+});
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    transition: '0.1s',
+    opacity: 1
+  });
+});
+
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+new Swiper('.promotion .swiper-container', {
+  direction: 'horizontal',
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000 //ms기준 500은 0.5초
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택지
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation:{
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  }
+});
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function(){
+  isHidePromotion = !isHidePromotion
+  if(isHidePromotion){
+    //숨김 처리
+    promotionEl.classList.add('hide');
+  }
+  else{
+    //보임 처리
+    promotionEl.classList.remove('hide');
+  }
+});
+
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    repeat: -1, // 무한반복
+    yoyo: true, // 한번 재생된 애니메이션을 다시 뒤집어서 한번 더 재생
+    ease: Power1.easeInOut,
+    delay: random(0, delay)
+  });
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', 0.5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: 0.8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
+
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear(); // 2022
